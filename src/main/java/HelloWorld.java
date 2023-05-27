0426e32950d0f289961c00df356a393676a4b27d
Daniel Stradowski <daniels@chatdesk.com>
2023-05-27 16:43:40 +0200

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Date currentTime = new Date();
+        System.out.println("The current time is: " + currentTime);
+
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
