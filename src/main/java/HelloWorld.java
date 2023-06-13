17e71ef5b96f5ac94810bee43f05fce47502ffff
Igor Salicki <you@example.com>
2023-06-13 20:24:32 +0200

    test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,7 +3,7 @@ import java.util.Date;
 public class HelloWorld {
     public static void main(String[] args) {
         Date currentTime = new Date();
-        System.out.println("The current time is: " + currentTime);
+        System.out.println("The current time is : " + currentTime);
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
