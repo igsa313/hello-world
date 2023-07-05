c5f40ef1bc741feaa7c721abe3a141c932e8fc20
Igor <isalicki@gmail.com>
2023-07-05 19:03:57 +0200

    Igor praca domowa

---
+++ src/main/java/Animal.java
@@ -0,0 +1,23 @@
+public class Animal {
+    public String getAnimalSound() {
+        return "The animal makes a sound";
+    }
+class Cat extends Animal {
+    public String getAnimalSound() {
+        return "The cat says: meow";
+    }
+}
+class Dog extends Animal {
+    public String getAnimalSound() {
+        return "The dog says: bark";
+    }
+}
+    class Turtle extends Animal {
+        public String getAnimalSound() {
+            return "The turtle says: oop";
+            int color;
+            int size;
+            int hardness;
+        }
+    }
+}
